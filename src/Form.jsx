0dd846e419406ebcd8523b6c06c2dfbf470bd4e3
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-07-18 16:17:50 +0300

    start

---
+++ src/Form.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useFormik } from "formik";
+
+const Form = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      occupation: "",
+    },
+    onSubmit: (values) => {
+      // Handle form submission
+    },
+    validate: (values) => {
+      // Perform form field validation
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <input
+        onChange={formik.handleChange}
+        value={formik.values.username}
+        type="text"
+        name="username"
+        placeholder="Name"
+      />
+      <input
+        onChange={formik.handleChange}
+        value={formik.values.occupation}
+        type="text"
+        name="occupation"
+        placeholder="Occupation"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
